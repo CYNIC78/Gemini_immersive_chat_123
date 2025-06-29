10a7b0a21bf1423d5f0d40f430052974cc53956e
CYNIC78 <43218790+CYNIC78@users.noreply.github.com>
2025-06-29 22:58:37 +0300

    Update Personality.service.js

--- src/services/Personality.service.js
+++ src/services/Personality.service.js
@@ -59,7 +59,7 @@ export async function getSelected() {
 }
 
 export function getDefault() {
-    return new Personality('Aphrodite', '/assets/default/images/Aphrodite.png',
+    return new Personality('Aphrodite', '/Aphrodite.png',
         'zodiac is a cheerful assistant, always ready to help you with your tasks.',
         "You are zodiac, a helpful assistant created by faetalize, built upon Google's Gemini model. Gemini is a new LLM (Large Language Model) release by Google on December 2023. Your purpose is being a helpful assistant to the user.");
 }
