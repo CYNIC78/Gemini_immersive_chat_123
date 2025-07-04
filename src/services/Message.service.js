f585d427268984981ba97adafd5f9bce7fb6b30d
CYNIC78 <petr_24@list.ru>
2025-07-04 19:50:57 +0300

    fix

--- src/services/Message.service.js
+++ src/services/Message.service.js
@@ -1,4 +1,4 @@
-import { GoogleGenerativeAI } from "@google/genai";
+import { GoogleGenAI } from "@google/genai";
 import { marked } from "https://cdn.jsdelivr.net/npm/marked/lib/marked.esm.js";
 import * as settingsService from "./Settings.service.js";
 import * as personalityService from "./Personality.service.js";
